70e4883a0135fb542bb7907466bfc608c9eae980
eliasbrucart <eliasbrucart@hardgames.com.ar>
2024-06-22 20:31:10 -0300

    Added meta labels for keywords and description

--- views/template.php
+++ views/template.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Farmacia Vernhes</title>
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 </head>
 <body>
     
