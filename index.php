cea6ec7dc50751dfb1d4cf58e2fbf39801ebdd82
eliasbrucart <eliasbrucart@hardgames.com.ar>
2024-06-22 20:29:37 -0300

    Added ini configs for error logs

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
-<?php 
+<?php
+
+ini_set('display_errors', 1);
+ini_set("log_errors", 1);
+ini_set("error_log",  "D:/xampp/htdocs/FarmaciaVernhes/php_error_log");
 
 require_once "controllers/template_controller.php";
 
