f1205b8071305503bd0d930bb2646ef783903d24
eliasbrucart <eliasbrucart@hardgames.com.ar>
2024-06-22 01:21:16 -0300

    Improved index

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Farmacia Vernhes</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+
+require_once "controllers/template_controller.php";
+
+$template = new TemplateController();
+$template->GetTemplate();
+
+?>
