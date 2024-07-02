17cb11c9db5919eca8bd54d9d0a73abb4ec63b93
eliasbrucart <eliasbrucart@hardgames.com.ar>
2024-07-02 01:29:52 -0300

    Added Turner Controller and Model class

--- index.php
+++ index.php
@@ -6,8 +6,11 @@ ini_set("error_log",  "D:/xampp/htdocs/FarmaciaVernhes/php_error_log");
 
 require_once "controllers/template_controller.php";
 require_once "controllers/pharmacy_controller.php";
+require_once "controllers/turner_controller.php";
 
 require_once "models/pharmacy_model.php";
+require_once "models/turner_model.php";
+
 require_once "models/routes.php";
 
 $template = new TemplateController();
